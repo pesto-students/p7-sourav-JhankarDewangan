5b43fb4221a6dd07fe7b871c23a0d43c944d8ccd
Jhankar Dewangan <jhankardew@gmail.com>
2023-01-14 22:39:29 +0530

    Completed Assignment of Week-8

---
+++ Week-8/Problem 7.3.js
@@ -0,0 +1,59 @@
+
+  
+
+let head; 
+  
+class Node 
+{
+    constructor(d)
+    {
+        this.data = d;
+        this.next = null;
+    }
+}
+  
+
+function push(new_data)
+{
+      
+    
+    let new_node = new Node(new_data);
+  
+    
+    new_node.next = head;
+  
+
+    head = new_node;
+}
+  
+function detectLoop()
+{
+    let slow_p = head, fast_p = head;
+    let flag = 0;
+      
+    while (slow_p != null && fast_p != null && 
+           fast_p.next != null)
+    {
+        slow_p = slow_p.next;
+        fast_p = fast_p.next.next;
+        if (slow_p == fast_p)
+        {
+            flag = 1;
+            break;
+        }
+    }
+    if (flag == 1)
+        document.write("Loop Found");
+    else
+        document.write("No Loop");
+}
+  
+
+push(20);
+push(4);
+push(15);
+push(10);
+  
+  
+detectLoop();
+  
